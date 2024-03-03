54517b90623912039b440974c07993b7b4e6f12d
Adam Hrouda <hroudaadam@gmail.com>
2024-03-03 15:21:21 +0100

    .

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
             
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
+            }, 750, 'easeInOutExpo');
             
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
@@ -50,7 +50,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 750, 'easeInOutExpo');
         return false;
     });
 
